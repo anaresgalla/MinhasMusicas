d90a0625bbcff7a35056c285fdb1c439a274c999
anaresgalla <anaresgalla@gmail.com>
2025-11-07 11:21:24 -0300

    Cria classe main

---
+++ src/br/com/alura/minhasmusicas/main/Main.java
@@ -0,0 +1,32 @@
+package br.com.alura.minhasmusicas.main;
+
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Main {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Statues");
+        minhaMusica.setArtista("Foo Fighters");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Medo e Delírio em Brasília");
+        meuPodcast.setApresentador("Cristiano e Pedro");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+    }
+}
